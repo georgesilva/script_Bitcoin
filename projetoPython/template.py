0a92bdc24bfbb0283bdfacbfd246474636d8bee6
georgesilva <georgelbsilva@gmail.com>
2015-10-17 01:04:05 -0300

    Update template.py

--- projetoPython/template.py
+++ projetoPython/template.py
@@ -4,7 +4,7 @@ import frbtclib
 
 wallet = 'ENDEREÇO DA SUA CARTEIRA BITCOIN'
 password = 'SENHA'
-startBet = 0.00000015 # Your balance must be 10^4 or 10^5 higher than this number. At least.
+startBet = 0.00000005 # Your balance must be 10^4 or 10^5 higher than this number. At least.
 stopPercentage = 0.03  # Reaching this percentage of your balance the script stops. If you dont want it, put "2". Recommended "0.08" or lower. 
 odds = '10'  # Your Payout
 lossMulti = 1.14  # On Loss Multiply to (1.14 is equal to "Increase in 14%")
